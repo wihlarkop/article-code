0220b99e412426d1f7acff5b6640753fd198b445
wihlarkop <wihlarko26@gmail.com>
2025-02-05 20:19:58 +0700

    - add print config

--- medium/how_to_load_toml_with_pydantic/main.py
+++ medium/how_to_load_toml_with_pydantic/main.py
@@ -1,4 +1,10 @@
+from dotenv_config import dot_env_settings
+from json_config import json_settings
+from toml_config import toml_settings
 from yaml_config import yaml_settings
 
 if __name__ == "__main__":
-    print(yaml_settings.database)
+    print(".env result", dot_env_settings)
+    print("config.toml result",toml_settings)
+    print("config.json result",json_settings)
+    print("config.yaml result",yaml_settings)
